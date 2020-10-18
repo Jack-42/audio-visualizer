6fa3f96106f3e9232d8981b53f1a194f91c0bed8
Jack-42 <jakob-robert@hotmail.de>
2020-10-18 23:18:03 +0200

    Play example audio file

--- public/src/app.js
+++ public/src/app.js
@@ -1,1 +1,24 @@
-console.log("Hello World!");
+let ctx;
+let audioElement;
+let playing;
+
+function init() {
+    audioElement = document.getElementById("test-audio");
+    audioElement.addEventListener("ended", () => {
+        console.log("Audio ended");
+    });
+
+    const AudioContext = window.AudioContext || window.webkitAudioContext; // for legacy browsers
+    ctx = new AudioContext();
+    const track = ctx.createMediaElementSource(audioElement);
+    track.connect(ctx.destination);
+}
+
+function play() {
+    audioElement.play();
+}
+
+function stop() {
+    audioElement.pause();
+    audioElement.currentTime = 0;
+}
