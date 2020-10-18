0e986804d73213222e1fd5a8fdcc523280505d2b
Jack-42 <jakob-robert@hotmail.de>
2020-10-19 00:02:11 +0200

    Add analyzer to get fft data, request it periodically

--- public/src/app.js
+++ public/src/app.js
@@ -1,24 +1,52 @@
-let ctx;
+const AudioContext = window.AudioContext || window.webkitAudioContext; // for legacy browsers
+
+const FFT_WINDOW_SIZE = 2048;
+
 let audioElement;
-let playing;
+let canvasCtx;
+
+let analyser;
+let fftBuffer;
+let processIntervalInMs;
+let timer;
 
 function init() {
     audioElement = document.getElementById("test-audio");
     audioElement.addEventListener("ended", () => {
-        console.log("Audio ended");
+        clearInterval(timer);
     });
 
-    const AudioContext = window.AudioContext || window.webkitAudioContext; // for legacy browsers
-    ctx = new AudioContext();
-    const track = ctx.createMediaElementSource(audioElement);
-    track.connect(ctx.destination);
+    const canvas = document.getElementById("spectrum-canvas");
+    canvasCtx = canvas.getContext("2d");
+
+    const ctx = new AudioContext();
+
+    analyser = ctx.createAnalyser();
+    analyser.fftSize = FFT_WINDOW_SIZE;
+    const bufferSize = analyser.frequencyBinCount;
+    fftBuffer = new Float32Array(bufferSize);
+
+    const source = ctx.createMediaElementSource(audioElement);
+    source.connect(analyser);
+    analyser.connect(ctx.destination);
+
+    processIntervalInMs = (FFT_WINDOW_SIZE / ctx.sampleRate) * 1000;
 }
 
 function play() {
     audioElement.play();
+    timer = setInterval(() => {
+        process();
+    }, processIntervalInMs);
 }
 
 function stop() {
     audioElement.pause();
     audioElement.currentTime = 0;
+    clearInterval(timer);
+}
+
+function process() {
+    analyser.getFloatFrequencyData(fftBuffer);
+    console.log(fftBuffer);
 }
